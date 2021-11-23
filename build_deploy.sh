5f7533e1820f209ce29f41c70203e2846874b3df
Johan Karpantschof <johan931@gmail.com>
2021-11-23 21:13:36 +0100

    deploy script

--- build_deploy.sh
+++ build_deploy.sh
@@ -2,6 +2,7 @@
 
 PROJECT_NAME=A2Sem3
 DROPLET_URL=karpantschof.com
+read -p "Press enter to deploy $PROJECT_NAME @ $DROPLET_URL"
 echo ##############################
 echo Building $PROJECT_NAME in ./build/
 echo ##############################
@@ -10,8 +11,7 @@ npm run build
 echo ##############################
 echo Deploying $PROJECT_NAME @ remote: /var/www/$PROJECT_NAME
 echo ##############################
-
+ssh root@$DROPLET_URL " bash -c \"mkdir /var/www/$PROJECT_NAME\" "
 scp -r ./build/* root@$DROPLET_URL:/var/www/$PROJECT_NAME
-
-# quick deploy singlefile
-# scp index.html root@karpantschof.com:/var/www/html
+echo ##############################
+read -p "Done. Press Enter to continue."
--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,11 @@
 
 PROJECT_NAME=A2Sem3
 DROPLET_URL=karpantschof.com
-echo ##############################
-echo Building $PROJECT_NAME in ./build/
-echo ##############################
-npm run build
+read -p "Press enter to deploy $PROJECT_NAME @ $DROPLET_URL"
 
 echo ##############################
 echo Deploying $PROJECT_NAME @ remote: /var/www/$PROJECT_NAME
 echo ##############################
-
+ssh root@$DROPLET_URL " bash -c \"mkdir /var/www/$PROJECT_NAME\" "
 scp -r ./build/* root@$DROPLET_URL:/var/www/$PROJECT_NAME
+read -p "Done. Press Enter to continue."
