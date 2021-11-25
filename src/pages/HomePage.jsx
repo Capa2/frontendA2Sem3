895ef1d5ada908e2123f225a238db9e6151b2aeb
KD131 <71063280+KD131@users.noreply.github.com>
2021-11-25 15:49:33 +0100

    adds dropdown to change search parameter to author

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Button, Form, FormControl, InputGroup } from "react-bootstrap";
+import { Button, ButtonGroup, Dropdown, Form, FormControl, FormSelect, InputGroup } from "react-bootstrap";
 import SearchResults from "../components/SearchResults";
 import { useNavigate } from "react-router";
 import { useSearchParams } from "react-router-dom";
@@ -8,11 +8,17 @@ export default function Home() {
     const [searchParams, setSearchParams] = useSearchParams();
     const query = searchParams.get("query");
     const [search, setSearch] = useState(!!query ? query : "");
+    const [searchType, setSearchType] = useState("");
     // ^ search could just initialise as query, but then it gives an annoying error message in console because the controlled value goes from undefined to defined.
     const navigate = useNavigate();
 
     function handleChange(event) {
         setSearch(event.target.value);
+    }
+
+    function changeSearchType(event) {
+        const newType = event.target.value;
+        setSearchType(newType);
     }
 
     function performSearch(event) {
@@ -30,7 +36,15 @@ export default function Home() {
         <Form className="mb-3" onChange={handleChange} onSubmit={performSearch}>
             <InputGroup>
                 <FormControl name="search" type="text" value={search} placeholder="Search for books" />
-                <Button type="submit">Search</Button>
+                <Dropdown as={ButtonGroup}>
+                    <Button type="submit">Search</Button>
+                    <Dropdown.Toggle split />
+                    <Dropdown.Menu onClick={changeSearchType}>
+                        <Dropdown.Item as="button" value="">Simple</Dropdown.Item>
+                        <Dropdown.Item as="button" value="author:">Author</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <InputGroup.Text>{searchType}</InputGroup.Text>
             </InputGroup>
         </Form>
         {query &&
