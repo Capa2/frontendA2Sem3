29bbfa887d96acb01184b2cd73252698a5af3281
KD131 <71063280+KD131@users.noreply.github.com>
2021-11-25 18:29:26 +0100

    puts all search parameters in url as &key=value

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,49 +6,60 @@ import { useSearchParams } from "react-router-dom";
 
 export default function Home() {
     const [searchParams, setSearchParams] = useSearchParams();
-    const query = searchParams.get("query");
-    const [search, setSearch] = useState(!!query ? query : "");
-    const [searchType, setSearchType] = useState("");
+    const queryObject = Object.fromEntries(searchParams.entries());
+    const [search, setSearch] = useState(queryObject);
     // ^ search could just initialise as query, but then it gives an annoying error message in console because the controlled value goes from undefined to defined.
     const navigate = useNavigate();
 
     function handleChange(event) {
-        setSearch(event.target.value);
+        const key = event.target.name;
+        const value = event.target.value;
+        setSearch({ ...search, [key]: value });
     }
 
     function changeSearchType(event) {
         const newType = event.target.value;
-        setSearchType(newType);
+        setSearch({ ...search, "type": newType });
     }
 
     function performSearch(event) {
         event.preventDefault();
-        if (search) {
+        if (search.query) {
+
+            let params = new URLSearchParams(search);
+
             navigate({
                 pathname: "search",
-                search: `query=${search}`
+                search: params.toString()
             });
         }
+    }
+
+    function getQueryString() {
+        const { type, query } = queryObject;
+        const res = (type ? type + ":" : "") + query;
+        console.log(res);
+        return res;
     }
 
     return <>
         <h1>Home</h1>
         <Form className="mb-3" onSubmit={performSearch}>
             <InputGroup>
-                <FormControl name="search" type="text" value={search} onChange={handleChange} placeholder="Search for books" />
+                <FormControl name="query" type="text" value={search.query ? search.query : ""} onChange={handleChange} placeholder="Search for books" />
                 <Dropdown as={ButtonGroup}>
                     <Button type="submit">Search</Button>
                     <Dropdown.Toggle split />
                     <Dropdown.Menu onClick={changeSearchType}>
                         <Dropdown.Item as="button" value="">Simple</Dropdown.Item>
-                        <Dropdown.Item as="button" value="author:">Author</Dropdown.Item>
+                        <Dropdown.Item as="button" value="author">Author</Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
-                <InputGroup.Text>{searchType}</InputGroup.Text>
+                <InputGroup.Text>{search.type}</InputGroup.Text>
             </InputGroup>
         </Form>
-        {query &&
-            <SearchResults query={query} />
+        {queryObject.query &&
+            <SearchResults query={getQueryString()} />
         }
     </>
 }
