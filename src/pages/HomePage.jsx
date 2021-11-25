1c4d49e3e49268cacf30ff44f90f68b0b16eb39a
Johan Karpantschof <johan931@gmail.com>
2021-11-25 18:20:46 +0100

    add book/author select

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,7 +8,7 @@ export default function Home() {
     const [searchParams, setSearchParams] = useSearchParams();
     const query = searchParams.get("query");
     const [search, setSearch] = useState(!!query ? query : "");
-    const [searchType, setSearchType] = useState("");
+    const [searchType, setSearchType] = useState();
     // ^ search could just initialise as query, but then it gives an annoying error message in console because the controlled value goes from undefined to defined.
     const navigate = useNavigate();
 
@@ -16,9 +16,11 @@ export default function Home() {
         setSearch(event.target.value);
     }
 
-    function changeSearchType(event) {
-        const newType = event.target.value;
-        setSearchType(newType);
+    function handleSearchType(event) {
+        const type = event.target.value;
+        setSearchType(type);
+        console.log("selected search type ", type);
+        console.log("Search type state is ", searchType);
     }
 
     function performSearch(event) {
@@ -41,9 +43,22 @@ export default function Home() {
                     name="search"
                     type="text"
                     value={search}
+                    size="lg"
                     onChange={handleChange}
                     placeholder="Search"
                 />
+                <Form.Select
+                    defaultValue={searchType}
+                    size="lg"
+                    aria-label="search select"
+                    onChange={handleSearchType}
+                >
+                    <option value="book">book</option>
+                    <option value="author">author</option>
+                </Form.Select>
+                <Button type="submit">Search</Button>
+                {/*  
+                !! This seems needlessly complicated and hides away critical information from the user !!
                 <Dropdown as={ButtonGroup}>
                     <Button type="submit">Search</Button>
                     <Dropdown.Toggle split />
@@ -53,6 +68,7 @@ export default function Home() {
                     </Dropdown.Menu>
                 </Dropdown>
                 <InputGroup.Text>{searchType}</InputGroup.Text>
+                */}
             </InputGroup>
         </Form>
         {query &&
