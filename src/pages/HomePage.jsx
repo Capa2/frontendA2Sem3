0d467b1c21f2e49dae678dc16559bd6aee4fc7cf
Capa2 <johan931@gmail.com>
2021-11-26 15:06:10 +0100

    disabled search button when no search input

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,71 +1,80 @@
-import { useState } from "react"
-import { Button, Form, FormControl, FormSelect, InputGroup, Row, FloatingLabel } from "react-bootstrap";
+import { useState } from "react";
+import {
+	Button,
+	Form,
+	FormControl,
+	FormSelect,
+	InputGroup,
+	Row,
+	FloatingLabel,
+} from "react-bootstrap";
 import SearchResults from "../components/SearchResults";
 import { useNavigate } from "react-router";
 import { useSearchParams } from "react-router-dom";
 
 export default function Home() {
-    const [searchParams, setSearchParams] = useSearchParams();
-    const queryObject = Object.fromEntries(searchParams.entries());
-    const [search, setSearch] = useState(queryObject);
-    // ^ search could just initialise as query, but then it gives an annoying error message in console because the controlled value goes from undefined to defined.
-    const navigate = useNavigate();
+	const [searchParams, setSearchParams] = useSearchParams();
+	const queryObject = Object.fromEntries(searchParams.entries());
+	const [search, setSearch] = useState(queryObject);
+	// ^ search could just initialise as query, but then it gives an annoying error message in console because the controlled value goes from undefined to defined.
+	const navigate = useNavigate();
 
-    function handleChange(event) {
-        const key = event.target.name;
-        const value = event.target.value;
-        setSearch({ ...search, [key]: value });
-    }
+	function handleChange(event) {
+		const key = event.target.name;
+		const value = event.target.value;
+		setSearch({ ...search, [key]: value });
+	}
 
-    function performSearch(event) {
-        event.preventDefault();
-        if (search.query) {
+	function performSearch(event) {
+		event.preventDefault();
+		if (search.query) {
+			let params = new URLSearchParams(search);
 
-            let params = new URLSearchParams(search);
+			navigate({
+				pathname: "search",
+				search: params.toString(),
+			});
+		}
+	}
 
-            navigate({
-                pathname: "search",
-                search: params.toString()
-            });
-        }
-    }
+	function getQueryString() {
+		const { type, query } = queryObject;
+		const res = (type ? type + ":" : "") + query;
+		return res;
+	}
 
-    function getQueryString() {
-        const { type, query } = queryObject;
-        const res = (type ? type + ":" : "") + query;
-        return res;
-    }
+	return (
+		<>
+			<h1>Home</h1>
 
-    return <>
-        <h1>Home</h1>
-
-        <Form className="mb-3" onSubmit={performSearch}>
-            <InputGroup>
-                <FormControl
-                    className="w-66"
-                    name="query"
-                    type="text"
-                    value={search.query ? search.query : ""}
-                    size="lg"
-                    onChange={handleChange}
-                    placeholder="Search"
-                />
-                <FloatingLabel controlId="floatingFilter" label="Filter">
-                <FormSelect
-                    name="type"
-                    defaultValue={search.type}
-                    size="lg"
-                    aria-label="search select"
-                    onChange={handleChange}
-                >     
-                    {/* The empty value is to use the API's generic search as opposed to forcing it to match title. */}
-                    <option value="">none</option>
-                    <option value="title">book</option>
-                    <option value="author">author</option>
-                    <option value="subject">subject</option>
-                </FormSelect> </FloatingLabel>
-                <Button type="submit">Search</Button>
-                {/*  
+			<Form className="mb-3" onSubmit={performSearch}>
+				<InputGroup>
+					<FormControl
+						className="searchInput w-66"
+						name="query"
+						type="text"
+						value={search.query ? search.query : ""}
+						size="lg"
+						onChange={handleChange}
+						placeholder="Search"
+					/>
+					<FloatingLabel controlId="floatingFilter" label="Filter">
+						<FormSelect
+							name="type"
+							defaultValue={search.type}
+							size="lg"
+							aria-label="search select"
+							onChange={handleChange}
+						>
+							{/* The empty value is to use the API's generic search as opposed to forcing it to match title. */}
+							<option value="">none</option>
+							<option value="title">book</option>
+							<option value="author">author</option>
+							<option value="subject">subject</option>
+						</FormSelect>
+					</FloatingLabel>
+					<Button disabled={search.query == null || search.query.length < 3} type="submit">Search</Button>
+					{/*  
                 !! This seems needlessly complicated and hides away critical information from the user !!
                 <Dropdown as={ButtonGroup}>
                     <Button type="submit">Search</Button>
@@ -77,10 +86,13 @@ export default function Home() {
                 </Dropdown>
                 <InputGroup.Text>{searchType}</InputGroup.Text>
                 */}
-            </InputGroup>
-        </Form>
-        {queryObject.query &&
-            <Row><SearchResults query={getQueryString()} /></Row>
-        }
-    </>
+				</InputGroup>
+			</Form>
+			{queryObject.query && (
+				<Row>
+					<SearchResults query={getQueryString()} />
+				</Row>
+			)}
+		</>
+	);
 }
