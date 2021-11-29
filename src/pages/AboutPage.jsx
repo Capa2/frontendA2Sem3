076a1bae022195c843d8dc65da22e6540325a888
Johan Karpantschof <johan931@gmail.com>
2021-11-29 10:41:40 +0100

    logo

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -5,7 +5,7 @@ export default function About() {
         <>
             <h1>About us</h1>
             <p>Content about us goes here.</p>
-            
+            <div>Icons made by <a href="https://www.flaticon.com/authors/webalys" title="Webalys">Webalys</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
         </>
     );
 }
