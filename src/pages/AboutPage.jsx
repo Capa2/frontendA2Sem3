0f9ce8270403330d82c0e5a520056a9ffe02eb03
Johan Karpantschof <johan931@gmail.com>
2021-12-10 09:08:46 +0100

    about us

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,7 +2,9 @@ export default function About() {
     return (
         <>
             <h1>About us</h1>
-            <p>Content about us goes here.</p>
+            <p>This was developed by Johan, Kasper og Alex</p>
+            <a href="https://github.com/Capa2/frontendA2Sem3">Frontend sourcecode</a>
+            <a href="https://github.com/Capa2/BackendA2Sem3">Backend sourcecode</a>
             <div>Icons made by <a href="https://www.flaticon.com/authors/webalys" title="Webalys">Webalys</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
         </>
     );
